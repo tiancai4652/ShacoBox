036822219fb597e62b7226fc0f25363d7dcbaac3
Shaco <zhangranmm@126.com>
2019-01-16 09:59:23 +0800

    1 使其在所有窗口最上面 2 添加ESC关闭快捷键

---
+++ ShacoBox/ShacoBox/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShacoBox
+{
+    public class Configuration
+    {
+        public static string ShowFormat = "HH:MM:ss";
+    }
+}
--- ShacoBox/ShacoBox/MainWindow.xaml.cs
+++ ShacoBox/ShacoBox/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Windows;
@@ -9,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -28,6 +30,78 @@ namespace ShacoBox
             TimeUpdateThread.IsBackground = true;
             TimeUpdateThread.Start();
             this.DataContext = this;
+
+            IntPtr hWnd = new WindowInteropHelper(Application.Current.MainWindow).Handle;
+            IntPtr hWndProgMan = FindWindow("Progman", "Program Manager");
+            SetParent(hWnd, hWndProgMan);
+            var handle = new WindowInteropHelper(Application.Current.MainWindow).Handle;
+            IntPtr hprog = FindWindowEx(
+                FindWindowEx(
+                    FindWindow("Progman", "Program Manager"),
+                    IntPtr.Zero, "SHELLDLL_DefView", ""
+                ),
+                IntPtr.Zero, "SysListView32", "FolderView"
+            );
+            SetWindowLong(handle, GWL_HWNDPARENT, hprog);
+        }
+
+        string _ShowTime;
+        public string ShowTime
+        {
+            get
+            {
+                return _ShowTime;
+            }
+            set
+            {
+                _ShowTime = value;
+                OnPropertyChanged("ShowTime");
+            }
+        }
+        public Thread TimeUpdateThread { get; set; }
+        private void RealTimeUpdate()
+        {
+            while (true)
+            {
+                ShowTime = DateTime.Now.ToString(Configuration.ShowFormat);
+                Thread.Sleep(100);
+            }
+        }
+
+
+
+        #region Import
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpWindowClass, string lpWindowName);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
+        const int GWL_HWNDPARENT = -8;
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        #endregion
+
+        #region Event
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key.Equals(Key.Escape))
+            {
+                if (MessageBox.Show("确认退出吗?", "温馨提示", MessageBoxButton.OKCancel).Equals(MessageBoxResult.OK))
+                {
+                    if (TimeUpdateThread.IsAlive)
+                    {
+                        TimeUpdateThread.Abort();
+                    }
+                    while (TimeUpdateThread.ThreadState != ThreadState.Aborted)
+                    {
+                        Thread.Sleep(100);
+                    }
+                    this.Close();
+                }
+            }
         }
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -36,26 +110,11 @@ namespace ShacoBox
                 DragMove();
             }
         }
-        string _ShowTime;
-        public string ShowTime
-        {
-            get
-            { return _ShowTime; }
-            set
-            {
-                _ShowTime = value;
-                OnPropertyChanged("ShowTime");
-            }
-        }
-        public Thread TimeUpdateThread { get; set; }
-        void RealTimeUpdate()
-        {
-            while (true)
-            {
-                ShowTime = DateTime.Now.ToString("HH:MM:ss ");
-                Thread.Sleep(100);
-            }
-        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string propertyName = "")
         {
@@ -65,5 +124,6 @@ namespace ShacoBox
                 handler(this, new PropertyChangedEventArgs(propertyName));
             }
         }
+        #endregion
     }
 }
